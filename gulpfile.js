a17f37a088b0b44800a4e568dbd33508eb851976
andela-pbirir <purity.birir@andela.com>
2017-01-02 14:20:01 +0300

    Add functionality to the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,31 @@
 var gulp = require('gulp');
+    uglify=require('gulp-uglify'),
+    sass=require('sass')
+    ;
 
-gulp.task('default',function(){
-    console.log('Hello World');
+//Scripts task
+//Uglifies.
+gulp.task('scripts', function(){
+    gulp.src('src/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'))
 });
+
+//Styles task
+//Uglifies
+gulp.task('styles', function(){
+    gulp.src('src/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css/'))
+
+});
+
+//Watch task
+//Watches js
+gulp.task('watch', function(){
+    gulp.watch('src/*.js',['scripts'])
+});
+
+
+gulp.task('default', ['scripts','styles','watch']);
+   
