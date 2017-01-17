3073df2799124fac7d24ff94ae402e8d459b03a2
andela-pbirir <purity.birir@andela.com>
2017-01-17 14:58:44 +0300

    Add livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,26 @@
-var gulp = require('gulp');
-    uglify=require('gulp-uglify')
-    ;
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const plumber = require('gulp-plumber');
+const livereload = require('gulp-livereload');
 
 //Scripts task
-gulp.task('scripts', function(){
+gulp.task('scripts', function() {
     gulp.src('src/js/**/*.js')
-    .pipe(gulp.dest('src/js'));
+        .pipe(gulp.dest('src'))
+        .pipe(gulp.dest('build/js'))
+        .pipe(livereload());
 });
+
+//Tests task
 
 //Watch task
 //Watches js
-gulp.task('watch', function(){
-    gulp.watch('src/js/**/*.js',['scripts'])
+gulp.task('watch', function() {
+
+    var server = livereload();
+    livereload.listen();
+    gulp.watch('src/**/*.js', ['scripts'])
 });
 
 //Default task that runs all the scripts at the same time
-gulp.task('default', ['scripts','watch']);
-   
+gulp.task('default', ['scripts', 'watch']);
