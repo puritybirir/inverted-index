cef69d5e972fe93e928ae3db48d4ab46768cb738
andela-pbirir <purity.birir@andela.com>
2017-01-18 21:21:13 +0300

    Add populate index test

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -17,10 +17,62 @@ describe("Inverted index", () => {
     describe("Read book data", () => {
         let readData;
         beforeEach(() => {
-            readData = index.getIndex(myDoc);
+            readData = index.createIndex(myDoc);
         });
         it("assert JSON file is not empty", () => {
             expect(readData).not.toBe(null);
+        });
+    });
+
+    describe("Populate Index", function() {
+        let populatedData;
+        let mapData;
+        beforeEach(() => {
+            populatedData = index.getIndex('myDoc');
+            mapData = index.createIndex(myDoc);
+        });
+
+        it("index should be created after reading", () => {
+
+            expect(populatedData).not.toBe(null);
+
+        });
+        it("ensures a correct index is created", () => {
+            expect(mapData).toEqual(
+                [{
+                    alice: [0],
+                    in: [0],
+                    wonderland: [0],
+                    falls: [0],
+                    into: [0],
+                    a: [0, 1],
+                    rabbit: [0],
+                    hole: [0],
+                    and: [0, 1],
+                    enters: [0],
+                    world: [0],
+                    full: [0],
+                    of: [0, 1],
+                    imagination: [0],
+                    the: [1],
+                    lord: [1],
+                    rings: [1],
+                    fellowship: [1],
+                    ring: [1],
+                    an: [1],
+                    unusual: [1],
+                    alliance: [1],
+                    man: [1],
+                    elf: [1],
+                    dwarf: [1],
+                    wizard: [1],
+                    hobbit: [1],
+                    seek: [1],
+                    to: [1],
+                    destroy: [1],
+                    powerful: [1]
+                }]
+            );
         });
     });
 });
