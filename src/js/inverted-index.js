feb7da23a93d588d3b3d89f7e2baa84a5ff920a5
andela-pbirir <purity.birir@andela.com>
2017-02-06 18:52:25 +0300

    Refactored code according to checkpoint defense feedback

--- src/js/inverted-index.js
+++ src/js/inverted-index.js
@@ -16,8 +16,9 @@ class Index {
   /**
    * checkInputData
    *
-   * @param{object}
+   * Parses JSON if file type is correct
    *
+   * @param{object}
    * @return{bool}
    */
   checkInputData(file) {
@@ -30,24 +31,24 @@ class Index {
   }
 
   /**
-   * readFile
+   * rvalidJson
    *
    * Reads the JSON data using FileReader
    *
    * @param {object} file - Content of the file
+   * @param {string} filename - Name of the file
+   * @return {string}
    */
-  validJson(file, filename) {
+  validJson(filename, file) {
     if (!file.length) {
       return 'Empty file';
     }
     try {
       const jsonData = JSON.parse(file);
-
       if (this.checkTitleAndText(jsonData)) {
         return 'No title or text';
       }
-
-      this.createIndex(jsonData, filename);
+      this.createIndex(filename, jsonData);
       this.jsonData = jsonData;
       return 'File Uploaded';
     } catch (error) {
@@ -58,47 +59,67 @@ class Index {
   /**
    * cleanData
    *
-   * Reads the JSON data using FileReader
+   * Tokenizes data
    *
-   * @param {array}
+   * @param {string}
+   * @return {object}
    */
   cleanData(data) {
     let terms = (data)
       .toLowerCase()
-      .replace(/,(?=\S)/g, ' ')
-      .replace(/\btitle\b|\btext\b|,(?=\s)|[:.{}""]/g, '')
+      .replace(/[^a-z0-9 ]/gi, '')
       .split(' ');
     return terms;
   }
 
   /**
-   *checkTitleAndText
+   *  checkTitleAndText
    *
    * Checks that the array has a title and a text.
    *
    * @param {array}
+   * @return {bool}
    */
-  checkTitleAndText(array) {
-    return array.some(value => !value.hasOwnProperty('title') || !value.hasOwnProperty('text'))
+  checkTitleAndText(myArray) {
+    return myArray.some(value => !value.hasOwnProperty('title') || !value.hasOwnProperty('text'))
   }
 
+  /**
+   * getUniqueTerms
+   *
+   * Gets the unique terms.
+   *
+   * @param {array}
+   * @return {object}
+   */
   getUniqueTerms(terms) {
     return terms.filter((value, index, arr) => arr.indexOf(value) === index);
   }
 
-  getTerms() {
-    //
-  }
-
-  createIndexTerms(jsonData) {
-    let indexTerms;
-    jsonData.forEach((obj) => {
-      Object.keys(obj).forEach((key) => {
-        const terms = this.cleanData(obj[key]);
-        indexTerms = indexTerms.concat(this.getUniqueTerms(terms));
-      });
-    });
-    return indexTerms;
+  /**
+   * getValues
+   *
+   * Gets the values for each of the terms.
+   *
+   * @param {object}
+   * @param {string}
+   * @return {array}
+   */
+  getValues(jsonData, indexTerm) {
+    const objArray = [];
+    let i = 0;
+    for (const object of jsonData) {
+      for (const key in object) {
+        if (object.hasOwnProperty(key)) {
+          const terms = this.cleanData(object[key]);
+          if (terms.indexOf(indexTerm) > -1 && objArray.indexOf(i) < 0) {
+            objArray.push(i);
+          }
+        }
+      }
+      i += 1;
+    }
+    return objArray;
   }
 
   /**
@@ -106,43 +127,23 @@ class Index {
    *
    * Ensures that a correct Index is created
    *
-   * @param {array}
    * @param {object}
+   * @param {string}
    */
-  createIndex(jsonData, filename) {
+  createIndex(filename, jsonData) {
     if (jsonData != null) {
       let indexTerms = [];
       const index = {};
-
-      jsonData.forEach((obj) => {
+      jsonData.forEach((obj, objIndex) => {
         Object.keys(obj).forEach((key) => {
           const terms = this.cleanData(obj[key]);
           indexTerms = indexTerms.concat(this.getUniqueTerms(terms));
         });
       });
-
-      // indexTerms.forEach((term) => {
-      //   const objArray = [];
-      //   let i = 0;
-
-      // })s
-
-      for (const indexTerm of indexTerms) {
-        const objArray = [];
-        let i = 0;
-        for (const object of jsonData) {
-          for (const key in object) {
-            if (object.hasOwnProperty(key)) {
-              const terms = this.cleanData(object[key]);
-              if (terms.indexOf(indexTerm) > -1 && objArray.indexOf(i) < 0) {
-                objArray.push(i);
-              }
-            }
-          }
-          i += 1;
-        }
+      indexTerms.forEach((indexTerm) => {
+        let objArray = this.getValues(jsonData, indexTerm);
         index[indexTerm] = objArray;
-      }
+      });
       this.indexObject[filename] = index;
     }
   }
@@ -150,20 +151,44 @@ class Index {
   /**
    * getIndex
    *
-   * gets the created Index from createIndex
+   * Gets the created Index from createIndex
    *
    * @param {object} file - Content of the file
+   * @return {object}
    */
   getIndex(file) {
     return this.indexObject;
   }
 
   /**
+   * getResult
+   *
+   * Searches the created index
+   *
+   * @param {string}
+   * @param {string}
+   * @preturn {object}
+   */
+  getResult(items, filename) {
+    const fileResult = {};
+    for (const item of items) {
+      if (item !== '') {
+        if (this.indexObject[filename][item.toLowerCase()] !== undefined) {
+          fileResult[item] = this.indexObject[filename][item.toLowerCase()];
+        } else {
+          fileResult[item] = [];
+        }
+      }
+    }
+    return fileResult;
+  }
+
+  /**
    * searchIndex
    *
-   * searches the created Index
+   * Searches the created Index
    *
-   * @param {object}
+   * @param {string}
    * @param {array}
    *
    * @return{object}
@@ -172,33 +197,13 @@ class Index {
     if (Object.keys(this.indexObject).length !== 0) {
       const results = {};
       let items = valuesToSearch.toString();
-      items = items.replace(/[^a-z0-9 ]/gi, '').split(' ');
+      items = this.cleanData(items);
       if (Object.keys(this.indexObject).indexOf(filename) != -1) {
-        const fileResult = {};
-        for (const item of items) {
-          if (item !== '') {
-            if (this.indexObject[filename][item.toLowerCase()] !== undefined) {
-              fileResult[item] = this.indexObject[filename][item.toLowerCase()];
-            } else {
-              fileResult[item] = [];
-            }
-          }
-        }
-        results[filename] = fileResult;
+        results[filename] = this.getResult(items, filename);
       } else {
         if (filename === 'All') {
           for (const file of Object.keys(this.indexObject)) {
-            const fileResult = {};
-            for (const item of items) {
-              if (item !== '') {
-                if (this.indexObject[file][item.toLowerCase()] !== undefined) {
-                  fileResult[item] = this.indexObject[file][item.toLowerCase()];
-                } else {
-                  fileResult[item] = [];
-                }
-              }
-            }
-            results[file] = fileResult;
+            results[file] = this.getResult(items, file);
           }
         }
       }
