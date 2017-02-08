dcee04260375f15fd272bb3f8f88e11bc9511f91
andela-pbirir <purity.birir@andela.com>
2017-02-08 22:09:41 +0300

    Add newline at end of file

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,13 @@
-const gulp = require('gulp'),
-  uglify = require('gulp-uglify'),
-  plumber = require('gulp-plumber'),
-  livereload = require('gulp-livereload'),
-  jasmine = require('gulp-jasmine');
+const gulp = require('gulp');
+const livereload = require('gulp-livereload');
+const jasmine = require('gulp-jasmine');
 const browsersync = require('browser-sync').create();
 
-// Scripts task
 gulp.task('scripts', () => {
   gulp.src('src/js/**/*.js')
     .pipe(livereload());
 });
 
-// Front task
 gulp.task('Front', () => {
   browsersync.init({
     server: {
@@ -26,23 +22,15 @@ gulp.task('Front', () => {
   gulp.watch('./src/**/*.{css,js}').on('change', browsersync.reload);
 });
 
-
-
-
-//Tests task
 gulp.task('test', () => {
   gulp.src('jasmine/spec/*.js')
     .pipe(jasmine());
 });
 
-
-// Watch task
-// Watches js
 gulp.task('watch', () => {
-  const server = livereload();
+  livereload();
   livereload.listen();
   gulp.watch('src/**/*.js', ['scripts']);
 });
 
-// Default task that runs all the scripts at the same time
 gulp.task('default', ['scripts', 'watch', 'Front']);
